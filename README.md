9ee9417417d8c3a57f839c1ed8ec5a4d430de6c3
Kris-Chil <Kristinemchilson@gmail.com>
2026-05-10 22:09:50 +0200

    Login and readme

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# AW35-Assignment
+Render URL:
+
+https://aw35-assignment.onrender.com
+
+
+.env file
+
+ADMIN_USERNAME = avnadmin
+ADMIN_PASSWORD = AVNS__lnBI8wrWgL0IVyqKJ3
+DATABASE_NAME = defaultdb
+DIALECT = mysql
+DIALECTMODEL = mysql2
+DB_PORT = 22872
+HOST = aw35-assignment-kristinemchilson-4c09.j.aivencloud.com
--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,41 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
 
+router.post('/login', (req, res, next) => {
+  try {
+    const { username, password } = req.body;
+    userService.getOne(username).then((dbUser) => {
+      if (dbUser == null) return res.status(404).json({ error: 'User not found or password incorrect' });
+
+      crypto.pbkdf2(password, dbUser.dataValues.Salt, 310000, 32, 'sha256', function (err, hashedPassword) {
+        if (err) {
+          return cb(err);
+        }
+        if (!crypto.timingSafeEqual(dbUser.dataValues.EncryptedPassword, hashedPassword)) {
+          return res.status(401).send({ result: 'Incorrect email or password' });
+        }
+        let token;
+        try {
+          token = jwt.sign({ id: dbUser.dataValues.Id, username: dbUser.dataValues.Username }, process.env.TOKEN_SECRET, { expiresIn: '2h' });
+        } catch (err) {
+          return res.status(400).send({
+            result: 'Something went wrong when creating JWT token',
+          });
+        }
+        return res.status(200).send({
+          message: 'User logged in successfully',
+          data: {
+            token: token,
+            username: dbUser.dataValues.Username,
+          },
+        });
+      });
+    });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
 const EncodeJWT = (id, username) => {
   try {
     token = jwt.sign({ id: id, username: username }, process.env.TOKEN_SECRET, { expiresIn: '2h' });
