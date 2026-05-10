1a87cc23230d68cb86e587773a6224bf7c20fe8f
Kris-Chil <Kristinemchilson@gmail.com>
2026-05-10 20:50:14 +0200

    env added for Render

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
@@ -7,6 +8,9 @@ var logger = require('morgan');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var participantRouter = require('./routes/participant');
+
+var db = require("./models");
+db.sequelize.sync({ force: false })
 
 var app = express();
 
